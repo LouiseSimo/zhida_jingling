60a4f298003fb7764016e8a90d83932ce8fbf11f
LouiseSimo <76173882+LouiseSimo@users.noreply.github.com>
2025-06-15 19:05:20 +0200

    updates and responsive modified

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { AppNavigationComponent } from './app-navigation/app-navigation.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MaterialModule } from './material/material.modules';
-import { AppHeader } from "./app-header /app-header";
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
     AppNavigationComponent,
     MatIconModule,
-    MaterialModule, AppHeader],
+    MaterialModule,],
 
   templateUrl: './app.html',
   styleUrl: './app.scss'
